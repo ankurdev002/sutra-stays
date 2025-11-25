fdc20c1a581cc1e24a8182b3f313e9cd9c3c073b
ankurdev002 <ankursaini795@gmail.com>
2025-11-25 23:31:05 +0530

    Refactor Navbar component for improved mobile responsiveness
    - Added a hamburger menu icon for mobile view, allowing users to toggle the menu.
    - Implemented state management for menu visibility using useState.
    - Adjusted layout styles for better alignment and responsiveness across devices.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,14 @@
+"use client";
 import Image from "next/image";
 import Button from "./Button";
 import navlinks from "@/constants/navlinks";
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div className="fixed top-0 z-50 w-full h-23 bg-[#00000066] flex justify-between items-center px-10 drop-shadow-md">
+    <div className="fixed top-0 left-0 right-0 z-50 h-23 bg-[#00000066] flex justify-between items-center px-10 drop-shadow-md">
       <div>
         <Image
           src="/assets/logo/nav-logo.svg"
@@ -14,20 +17,47 @@ const Navbar = () => {
           height={100}
         />
       </div>
-      <div className="flex-1 flex gap-9.5 items-center justify-center">
-        {
-          navlinks.map((link) => (
-            <Link href={link.href} key={link.label} className="text-bone-white text-sm font-medium font-inter px-4">
-              <span className="text-bone-white text-sm font-medium font-inter hover:text-deep-green transition-all duration-300">
-                {link.label}
-              </span>
-            </Link>
-          ))
-        }
+      <div className="lg:hidden">
+        <Image
+          src="/assets/icons/ham.svg"
+          alt="menu"
+          width={40}
+          height={40}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        />
       </div>
-      <div className="flex gap-2 items-center justify-center">
-        <Button variant="off-white" className="rounded-full text-black border-0" label="Book Now" />
-        <Button variant="transparent" className="rounded-full" label="EN" icon={<Image src="/assets/icons/down-arrow.svg" alt="en" width={16} height={16} />} />
+      <div className="hidden lg:flex flex-1 gap-9.5 items-center justify-center">
+        {navlinks.map((link) => (
+          <Link
+            href={link.href}
+            key={link.label}
+            className="text-bone-white text-sm font-medium font-inter px-4"
+          >
+            <span className="text-bone-white text-sm font-medium font-inter hover:text-deep-green transition-all duration-300">
+              {link.label}
+            </span>
+          </Link>
+        ))}
+      </div>
+      <div className="hidden lg:flex gap-2 items-center justify-center">
+        <Button
+          variant="off-white"
+          className="rounded-full text-black border-0"
+          label="Book Now"
+        />
+        <Button
+          variant="transparent"
+          className="rounded-full"
+          label="EN"
+          icon={
+            <Image
+              src="/assets/icons/down-arrow.svg"
+              alt="en"
+              width={16}
+              height={16}
+            />
+          }
+        />
       </div>
     </div>
   );
